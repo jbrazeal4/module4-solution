adfa02021415add5f478a5b9eaf7ca44a9a9026a
jbrazeal4 <124006363+jbrazeal4@users.noreply.github.com>
2023-04-22 12:32:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -39,8 +39,7 @@ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula"
 // using either the helloSpeaker's or byeSpeaker's 'speak' method.
 // See Lecture 50, part 1
 for (var i = 0; i <names.length; i++) {
- var name = names[i];
- helloSpeaker.speak("John");
+ console.log(names[i]);
  
   // STEP 11:
   // Retrieve the first letter of the current name in the loop.
